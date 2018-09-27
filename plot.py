091e16cbda52591ebbc6105e536bf92bf8256221
ronrest <ronny.coding@gmail.com>
2018-09-27 19:29:52 +1000

    FEAT: PlotGroup now accepts `share` parameter

--- plot.py
+++ plot.py
@@ -93,10 +93,12 @@ class ProtoPlot(object):
         return x
 
 class PlotGroup(object):
-    def __init__(self, items=[], engine="mpl", type="overlay"):
+    def __init__(self, items=[], engine="mpl", type="overlay", share=True):
+        # TODO: implement the use of share being passed as parameter
         self.items = items
         self.type = type
         self.engine = engine
+        self.share = share
 
     def compile(self, engine=None):
         if engine is None:
