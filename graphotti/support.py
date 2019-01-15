a15f5c2804174572803a45a22b6f01ce696b073b
ronrest <ronny.coding@gmail.com>
2019-01-15 13:05:10 +1100

    FEAT: Add color_range()

--- graphotti/support.py
+++ graphotti/support.py
@@ -1,3 +1,5 @@
+import matplotlib
+
 def rescale(x, range, newrange=(0,1)):
     mina, maxa = range
     minb, maxb = newrange
@@ -21,4 +23,13 @@ def rgb2hex(x, alpha=False):
     b = "{:02x}".format(int(x[2]*multiplier))
     return ("#"+r+g+b).upper()
 
+
+# # COLOR RANGE
+def color_range(n, cmap="YlOrRd"):
+    """ takes a matplotlib colormap name and returns a list of n RGB values
+        interpolated alond that colormap
+    """
+    cmap = matplotlib.cm.get_cmap(cmap)
+    c = [rgb2hex(cmap(x)) for x in np.linspace(0,1,n)]
+    return c
 
