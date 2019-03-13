8bfb60712fd579492122420403333b17228b6032
ronrest <ronny.coding@gmail.com>
2019-03-13 12:25:38 +1100

    TODO: set position of ticks in plots with multiple y axes

--- TODOS.py
+++ TODOS.py
@@ -1,1 +1,7 @@
 # TODO: interpret the scaly property in matplotlib engine
+# TODO: for non-shared axes, have option to position the ticks and labels
+#       on different sides of the plot.
+#       Either:
+#       1. Automatically alternate sides
+#       2. Have a property in each individual plot or plotgroup that
+#          allows you to manually set the side you want the ticks to be in.
