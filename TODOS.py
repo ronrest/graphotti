5212360d13c125c278245fb61211b524b0f9f95b
ronrest <ronny.coding@gmail.com>
2019-03-13 15:29:12 +1100

    TODO: BUG: index error when plotting more than 2 plots with independent y axes

--- TODOS.py
+++ TODOS.py
@@ -1,3 +1,14 @@
+# BUG: index error is thrown when using more than 2 plots, with independent y axes.
+#      Seems to be an issue with the scaley list in the `create_figure()` function
+#
+#     Example:
+#     a = gh.line([1,2,3,4,5,6,7])
+#     b = gh.line([2,3,4,5,6,7,8])
+#     c = gh.line([3,4,5,6,7,8,9])
+#     g = a-b-c
+#     g.plot("ply")
+#
+
 # TODO: interpret the scaly property in matplotlib engine
 # TODO: for non-shared axes, have option to position the ticks and labels
 #       on different sides of the plot.
